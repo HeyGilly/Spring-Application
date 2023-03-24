f9ddc880ec4582c609e690691956e1ba6642b442
gilly <nick.gilmore20@gmail.com>
2023-03-24 09:57:01 -0500

    Initial Commit

---
+++ src/test/java/com/heygilly/springbootapplication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heygilly.springbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
